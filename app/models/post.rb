d1356616f6a01b347cccacfc3503ff09a14aa3e6
Emma Perlman <emmajperlman@gmail.com>
2022-09-10 16:25:05 -0700

    commit tree ab773f9fad58cda788575d034a9f913fea4393bf

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: {in:["Fiction","Non-Fiction"]}
+
+    validate :click_bait
+
+    def click_bait
+        titles=["Won't Believe", "Secret", "Top","Guess"].map {|word| word.downcase}
+        if title
+            if titles.none? {|word| title.downcase.include? word} 
+                errors.add(:title, "isn't catchy enough")
+            end
+        end
+    end
+
 end
